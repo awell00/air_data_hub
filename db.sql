a174a3d47237015edc3cd4eb3667b24c7df1518c
awell00 <awellpro@gmail.com>
2024-04-02 13:42:18 +0200

    Update

--- db.sql
+++ db.sql
@@ -9,4 +9,4 @@ CREATE TABLE properties (
     value TEXT NOT NULL
 );
 
-INSERT INTO properties (name, value) VALUES ('version', '1.0.0');
+INSERT INTO properties (name, value) VALUES ('version', '2.0.0');
