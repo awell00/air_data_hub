4745fa01877750f3c141bb03fede54ac57c9c2a6
Awell <awellpro@gmail.com>
2024-04-07 22:22:05 +0200

    Update

--- resources/js/components/Example.tsx
+++ resources/js/components/Example.tsx
@@ -10,6 +10,7 @@ import i18n from '../../../i18n';
 import styled from 'styled-components';
 
 type Coordinate = [number, number];
+type Zoom = number;
 
 Radar.initialize('prj_live_pk_e0443df992afd7bebd2ae00bfbc34e850a743aea');
 
@@ -20,27 +21,26 @@ const App: React.FC = () => {
     const [center, setCenter] = useState<Coordinate | undefined>();
     const { t } = useTranslation();
     const [mapInstance, setMapInstance] = useState<mapboxgl.Map | null>(null);
+    const [styleChanged, setStyleChanged] = useState(false);
+    const [styleChangedOnce, setStyleChangedOnce] = useState(false);
+    const [zoomValue, setZoom] = useState<Zoom>(3);
+    const [mapStyle, setMapStyle] = useState('mapbox://styles/mapbox/standard');
 
     useEffect(() => {
         const handleResize = () => {
-            if (map.current) {
-                // Adjust the size of the map
-                const width = window.innerWidth * 0.8; // 80% of window width
-                const height = window.innerHeight * 0.8; // 80% of window height
-                map.current.resize(); // This will make Mapbox GL JS update the map's size
+            const width = window.innerWidth * 0.8;
+            const height = window.innerHeight * 0.8;
+            map.current?.resize();
 
-                let divisor = 250;
-                if (window.innerWidth <= 1400) {
-                    divisor = 380; // Adjust this value as needed
-                } else if (window.innerWidth <= 600) {
-                    divisor = 400; // Adjust this value as needed
-                }
-
-                // Adjust the zoom level
-                const zoom = Math.min(width, height) / divisor;
-                console.log(zoom)
-                map.current.setZoom(zoom);
+            let divisor = 250;
+            if (window.innerWidth <= 600) {
+                divisor = 400;
+            } else if (window.innerWidth <= 1400) {
+                divisor = 380;
             }
+
+            setZoom(Math.min(width, height) / divisor);
+            map.current?.setZoom(zoomValue);
         };
 
 
@@ -67,114 +67,120 @@ const App: React.FC = () => {
                 }
 
                 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW5zaG9vZCIsImEiOiJjbHVsOG1vcTUwaGt1Mmlsd2h3dTM0ZzFvIn0.W-MBSYBmX_D_AqpOUoRAcA';
-                if (mapContainer.current) {
-                    map.current = new mapboxgl.Map({
-                        container: mapContainer.current,
-                        style: 'mapbox://styles/mapbox/standard',
-                        center: [lon, lat],
-                        zoom: 1.7
+                map.current = new mapboxgl.Map({
+                    container: mapContainer.current,
+                    style: mapStyle,
+                    center: [lon, lat],
+                    zoom: 1.7
+                });
+
+                map.current.on('load', () => {
+                    map.current?.on('zoom', () => {
+                        if (map.current) {
+
+                            if (map.current.getZoom() > zoomValue * 1.5) {
+                                if (!styleChanged && map.current?.isStyleLoaded()) {
+                                    setMapStyle('mapbox://styles/robinshood/clulbaf1200p601r2a317d1ju');
+                                    setTimeout(() => {
+                                        setStyleChanged(true);
+                                    }, 1000);
+                                }
+                            }
+                        }
                     });
 
                     if (map.current) {
-                        map.current.on('load', () => {
-                            if (map.current) {
-                                map.current.setFog({
-                                    color: 'rgb(186, 210, 235)',
-                                    'high-color': 'rgb(36, 92, 223)',
-                                    'horizon-blend': 0.002,
-                                    'space-color': 'rgb(11,11,25)',
-                                    "star-intensity": 0
-                                });
-
-                            }
-
-                            handleResize();
-
-                            /*setTimeout(() => {
-                                if (map.current) {
-                                    map.current.flyTo({
-                                        center: [lon, lat],
-                                        zoom: 4,
-                                        essential: true
-                                    });
-                                }
-                            }, 2000);*/
-
-                            const coordinates = [
-                                [-0.580816, 44.836151],
-                                [-75.5, 39],
-                                [-76.5, 38],
-                                [-0.580816 + 0.3, 44.836151 - 0.3],
-                                [-77.5, 37],
-                                [-78.5, 36],
-                                [-79.5, 35],
-                                [-80.5, 34],
-                                [-81.5, 33],
-                                [-82.5, 32]
-                            ];
-
-                            const validCoordinates = coordinates.filter(coordinate => coordinate !== undefined) as Coordinate[];
-
-                            // Create a GeoJSON feature collection
-                            const geojson: GeoJSON.FeatureCollection = {
-                                type: 'FeatureCollection',
-                                features: validCoordinates.map((coordinate, index) => ({
-                                    type: 'Feature',
-                                    geometry: {
-                                        type: 'Point',
-                                        coordinates: coordinate
-                                    },
-                                    properties: {
-                                        id: index,
-                                        radius: (index + 1) * 10
-                                    }
-                                }))
-                            };
-
-                            if (map.current) {
-
-                                if (!map.current.getSource('points')) {
-                                    map.current.addSource('points', {
-                                        type: 'geojson',
-                                        data: geojson
-                                    });
-                                }
-
-                                if (!map.current.getLayer('points')) {
-                                    map.current.addLayer({
-                                        id: 'points',
-                                        type: 'circle',
-                                        source: 'points',
-                                        paint: {
-                                            'circle-radius': [
-                                                'interpolate',
-                                                ['linear'],
-                                                ['zoom'],
-                                                2, 1,
-                                                3, 2,
-                                                4, 5,
-                                                5, 10,
-                                                6, 15,
-                                                7, 20,
-                                                8, 30,
-                                                9, 40,
-                                                10, 60,
-                                                11, 100,
-                                                12, 200,
-                                                13, 300,
-                                                14, 400,
-                                                15, 500,
-                                                16, 700,
-                                            ],
-                                            'circle-color': '#007cbf'
-                                        }
-                                    });
-                                }
-                            }
+                        map.current.setFog({
+                            color: 'rgb(186, 210, 235)',
+                            'high-color': 'rgb(36, 92, 223)',
+                            'horizon-blend': 0.006,
+                            'space-color': 'rgb(11,11,25)',
+                            "star-intensity": 0
                         });
 
                     }
-                }
+
+                    handleResize();
+
+                    /*setTimeout(() => {
+                        if (map.current) {
+                            map.current.flyTo({
+                                center: [lon, lat],
+                                zoom: zoomValue * 1.5,
+                                essential: true
+                            });
+                        }
+                    }, 2000);*/
+
+                    const coordinates = [
+                        [-0.580816, 44.836151],
+                        [-75.5, 39],
+                        [-76.5, 38],
+                        [-0.580816 + 0.3, 44.836151 - 0.3],
+                        [-77.5, 37],
+                        [-78.5, 36],
+                        [-79.5, 35],
+                        [-80.5, 34],
+                        [-81.5, 33],
+                        [-82.5, 32]
+                    ];
+
+                    const validCoordinates = coordinates.filter(coordinate => coordinate !== undefined) as Coordinate[];
+
+                    // Create a GeoJSON feature collection
+                    const geojson: GeoJSON.FeatureCollection = {
+                        type: 'FeatureCollection',
+                        features: validCoordinates.map((coordinate, index) => ({
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: coordinate
+                            },
+                            properties: {
+                                id: index,
+                                radius: (index + 1) * 10
+                            }
+                        }))
+                    };
+
+                    if (!map.current?.getSource('points')) {
+                        map.current?.addSource('points', {
+                            type: 'geojson',
+                            data: geojson
+                        });
+                    }
+
+                    if (!map.current?.getLayer('points')) {
+                        map.current?.addLayer({
+                            id: 'points',
+                            type: 'circle',
+                            source: 'points',
+                            paint: {
+                                'circle-radius': [
+                                    'interpolate',
+                                    ['linear'],
+                                    ['zoom'],
+                                    2, 1,
+                                    3, 2,
+                                    4, 5,
+                                    5, 10,
+                                    6, 15,
+                                    7, 20,
+                                    8, 30,
+                                    9, 40,
+                                    10, 60,
+                                    11, 100,
+                                    12, 200,
+                                    13, 300,
+                                    14, 400,
+                                    15, 500,
+                                    16, 700,
+                                ],
+                                'circle-color': '#0b0b19'
+                            }
+                        });
+                    }
+                });
             } catch (error) {
                 console.error('Error fetching IP info:', error);
             }
@@ -186,11 +192,9 @@ const App: React.FC = () => {
         return () => {
             window.removeEventListener('resize', handleResize);
 
-            if (map.current) {
-                map.current.remove();
-            }
+            map.current?.remove();
         };
-    }, []);
+    }, [styleChanged, styleChangedOnce, zoomValue]);
 
     const [city, setCity] = useState('');
     const [lat, setLat] = useState(0);
@@ -216,13 +220,11 @@ const App: React.FC = () => {
     }, [city]);
 
     const flyToLocation = () => {
-        if (map.current) {
-            map.current.flyTo({
-                center: [lng, lat],
-                essential: true,
-                zoom: 8
-            });
-        }
+        map.current?.flyTo({
+            center: [lng, lat],
+            essential: true,
+            zoom: 8
+        });
     };
 
     return (
@@ -266,11 +268,13 @@ const Title = styled.h1`
     font-size: 8.6vw;
     font-family: "ArchivoBlack-Regular", sans-serif;
     letter-spacing: 1.8vw;
-    color: aliceblue;
+    color: #ffffff;
     padding: 30px;
     text-align: center;
     position: relative;
     z-index: 1;
+    text-shadow: 0px 0px 3px rgb(36, 92, 223), // Existing shadow
+    0px 0px 6px #BAD2EBFF;
 
     @media (max-width: 600px) {
         font-size: 7.5vw;
@@ -296,12 +300,16 @@ const Map = styled.div`
     right: 30px;
     top: 50%;
     transform: translateY(-50%);
-    border-radius: 0px;
+    border-radius: 20px;
     z-index: 0;
 
     @media (max-width: 1260px) {
         left: 0;
         right: 0;
         margin: auto;
+    }
+
+    @media (max-width: 500px) {
+        top: 40%;
     }
 `
